fc1f83f4bbce66e34093a9aaba97681948f31c49
Jacoby Broadnax <32787662+MashPop@users.noreply.github.com>
2019-03-13 21:02:39 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var menubtn = document.getElementsByClassName("menu");
 var closebtn = document.getElementsByClassName("close");
 var menu = document.getElementsByClassName("nav");
 var closebase = document.getElementsByClassName("close-base");
+var modal = document.getElementsByClassName("privacy-policy");
+var modalSpanner = document.getElementsByClassName("policy-spanner");
+var donation = document.getElementsByClassName("donate-sect")
 
 function expand() {
     menu[0].style.display = "block";
@@ -16,4 +19,14 @@ function collapse() {
     closebtn[0].style.display = "none";
     menu[0].style.display = "none";
     closebase[0].style.display = "none";
+}
+
+function modalOpen() {
+    modal[0].style.display = "block";
+    modalSpanner[0].style.display = "block";
+}
+
+function modalClose() {
+    modal[0].style.display = "none";
+    modalSpanner[0].style.display = "none";
 }
